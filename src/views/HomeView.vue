cfb2d673a790ee2b669145af3ca2b3a01ea4bc31
putri <@students.uin-suska.ac.id>
2023-04-11 00:25:00 +0700

    1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+
+    <HelloWorld msg="Dont procrastinate next time"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
